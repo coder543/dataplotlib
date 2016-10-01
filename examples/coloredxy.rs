de80297f334ca82a3cfa3881965cc92e0288e338
Piripant <piripant@gmail.com>
2016-10-01 13:18:11 +0200

    Added multiple plots support

--- examples/coloredxy.rs
+++ examples/coloredxy.rs
@@ -5,11 +5,23 @@ use dataplotlib::plotter::Plotter;
 
 fn main() {
     let x = linspace(0, 10, 100);
-    let y = x.iter().map(|x| x.sin()).collect();
-    let xy = zip2(&x, &y);
+    
+    let y_sin = x.iter().map(|x| x.sin()).collect();
+    let xy_sin = zip2(&x, &y_sin);
 
-    // Makes a new graph of the color [1.0, 1.0, 0.0, 1.0] (yellow)
-    let pb = PlotBuilder2D::simple_xy_colored(xy, [1.0, 1.0, 0.0, 1.0]);
+    let y_cos = x.iter().map(|x| x.cos()).collect();
+    let xy_cos = zip2(&x, &y_cos);
+
+    let xy_lin = zip2(&x, &x);
+
+    // Creates a new plot builder
+    let mut pb = PlotBuilder2D::new();
+    
+    // Adds the sin plot and the cos plot
+    pb.add_color_xy(xy_sin, [1.0, 0.0, 0.0, 1.0]);
+    pb.add_color_xy(xy_cos, [0.0, 1.0, 0.0, 1.0]);
+    pb.add_color_xy(xy_lin, [0.0, 0.0, 1.0, 1.0]);
+    
     let mut plt = Plotter::new();
     plt.plot2d(pb);
     plt.join();
